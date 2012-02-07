a58d8c8ca156630b8fe10de031e50c6ecd1f3c08
Sadan Masroor <sadanmasroor87@gmail.com>
2012-02-08 04:57:38 +0500

    Completed the level 3 and added the site layout

--- Gemfile
+++ Gemfile
@@ -1,16 +1,18 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.0'
-gem 'mysql2','0.2.7'
+gem 'rails', '3.0.1'
+gem 'heroku'
+gem 'rake','0.8.7'
  
 group :development do
   
    gem 'rspec-rails','2.0.0.beta.18'
+   gem 'mysql2','0.2.7'
    
  end
 
 group :test do
   
    gem 'rspec','2.0.0.beta.18'
-   
+   gem 'mysql2','0.2.7'
  end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe PagesController do
+
+render_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+    
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+    
+  end
+
+end
