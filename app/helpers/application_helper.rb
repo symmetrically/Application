b93e9d3939ca3437f155811ba5fba93235b67a61
Sadan Masroor <sadanmasroor87@gmail.com>
2012-03-19 03:36:17 +0500

    Added a title helper and style sheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  
+  # Return the title method in the application page
+  
+  def title
+    
+    base_title = "Ruby on Rails Tutorial Sample App"
+
+    if @title.nil?
+        base_title
+    else
+        "#{base_title} | #{@title}"
+    end
+    
+  end
+  
+  
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,10 @@ describe PagesController do
 
 render_views
 
+  before(:each) do
+      @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ render_views
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title",:content => "#{@base_title} | Home")
     end
     
     it "should have a non-blank body" do
@@ -30,7 +34,7 @@ render_views
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title",:content => "#{@base_title} | About")
     end
   end
 
@@ -42,7 +46,7 @@ render_views
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title",:content => "#{@base_title} | Contact")
     end
     
   end
